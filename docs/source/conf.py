9f607d46cd62324db71448ca66423d2f52e1990f
amagai-5 <samagai@s.h.k.u-tokyo.ac.jp>
2023-01-04 22:28:38 +0100

    rename file2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'TFSD-test'
-copyright = '2022, amagai'
-author = 'amagai'
+project = 'Lumache'
+copyright = '2021, Graziella'
+author = 'Graziella'
 
 release = '0.1'
 version = '0.1.0'
